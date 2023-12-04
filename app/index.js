05ffeb559424f0cb9579c44039352f03d7193a52
unbemperor <dwiprasetya1942@gmai.com>
2023-12-04 11:37:25 +0800

    modified:   app/index.js

--- app/index.js
+++ app/index.js
@@ -22,23 +22,22 @@ app.use((err, req, res, next) => {
 
 
 // If database is connected successfully, then run the server
-db_connection
-    .getConnection()
-    .then(() => {
-        app.listen(port, () => {
-            console.log(`Server is running on port ${port}`);
-        });
-    })
-    .catch((err) => {
-        console.log(`Failed to connect to the database: ${err.message}`);
-    });
-
-
-// app.listen(port, () =>
-//   console.log(`Listening on port ${port}`)
-// );
-
-//     // If database is connected successfully, then run the server
 // db_connection
-// .getConnection()
+//     .getConnection()
+//     .then(() => {
+//         app.listen(port, () => {
+//             console.log(`Server is running on port ${port}`);
+//         });
+//     })
+//     .catch((err) => {
+//         console.log(`Failed to connect to the database: ${err.message}`);
+//     });
+
+
+app.listen(port, () =>
+  console.log(`Listening on port ${port}`)
+);
+
+db_connection
+.getConnection()
 
